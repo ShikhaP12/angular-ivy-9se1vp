b1b8eb561e73a1da1ed3b804226b39398f790e82
ShikhaP12 <pshikha407@gmail.com>
2021-12-11 23:04:01 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { ReceipeComponent } from './receipes/receipes.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -14,6 +17,9 @@ import { ReceipeComponent } from './receipes/receipes.component';
     HelloComponent,
     HeaderComponent,
     ReceipeComponent,
+    ReceipeDetailComponent,
+    ReceipeListComponent,
+    ReceipeItemComponent,
   ],
   bootstrap: [AppComponent],
 })
