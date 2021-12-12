127b1e9d48fce8b893ad162bcb9b2d888861824a
ShikhaP12 <pshikha407@gmail.com>
2021-12-12 18:43:36 +0530

    2 files modified, 1 added

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../receipe.model';
 
 @Component({
   selector: 'app-receipe-list',
   templateUrl: './receipe-list.component.html',
 })
-export class ReceipeListComponent {}
+export class ReceipeListComponent implements OnInit {
+  receipes: Recipe[] = [
+    new Recipe(
+      'A Test Receipe',
+      'Test Description',
+      'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_08/1678882/steak-recipe-jc-210223.jpg'
+    ),
+  ];
+
+  constructor() {}
+  ngOnInit() {}
+}
