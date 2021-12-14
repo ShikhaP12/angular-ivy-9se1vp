a04f1837211f72eb83fffbbbe5a6601d6bb98136
ShikhaP12 <pshikha407@gmail.com>
2021-12-14 07:45:27 +0530

    3 files modified

--- src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
+++ src/app/receipes/receipe-list/receipe-item/receipe-item.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../../receipe.model';
 
 @Component({
   selector: 'app-receipe-item',
   templateUrl: './receipe-item.component.html',
 })
-export class ReceipeItemComponent {}
+export class ReceipeItemComponent implements OnInit {
+  @Input() receipe: Recipe;
+
+  constructor() {}
+  ngOnInit() {}
+}
