89d51bc207525e818fa8d400ddaca52b277b8e47
ShikhaP12 <pshikha407@gmail.com>
2021-12-14 08:23:58 +0530

    2 files modified

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -13,6 +13,11 @@ export class ReceipeListComponent implements OnInit {
       'Test Description',
       'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_08/1678882/steak-recipe-jc-210223.jpg'
     ),
+    new Recipe(
+      'Another Test Receipe',
+      'Test Description',
+      'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_08/1678882/steak-recipe-jc-210223.jpg'
+    ),
   ];
 
   constructor() {}
