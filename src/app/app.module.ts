7bc5376a401662bc6a47e50113cf4f3e6d05922e
ShikhaP12 <pshikha407@gmail.com>
2021-12-16 20:06:53 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -14,7 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [FormsModule, CommonModule, BrowserModule],
+  imports: [FormsModule, CommonModule, BrowserModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     HelloComponent,
