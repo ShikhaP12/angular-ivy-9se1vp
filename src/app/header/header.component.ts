fba05d2f155760c939e3d1d54865c2850b06ddde
ShikhaP12 <pshikha407@gmail.com>
2022-01-23 18:56:22 +0530

    2 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit {
+  private userSub: Subscription;
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe();
+  }
   onSaveData() {
     this.dataStorageService.storeReceipes();
   }
