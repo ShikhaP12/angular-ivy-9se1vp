c48daed37c7a76b65928a32d1c27c4f9369405fa
ShikhaP12 <pshikha407@gmail.com>
2022-01-23 19:19:45 +0530

    1 file modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
@@ -7,20 +13,27 @@ import { DataStorageService } from '../shared/data-storage.service';
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
+  isAuthenticated = false;
+
   constructor(
     private dataStorageService: DataStorageService,
     private authService: AuthService
   ) {}
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe();
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !user ? false : true;
+    });
   }
   onSaveData() {
     this.dataStorageService.storeReceipes();
   }
   onFetchData() {
     this.dataStorageService.fetchReceipes().subscribe();
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
